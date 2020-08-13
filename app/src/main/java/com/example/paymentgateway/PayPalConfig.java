18a2559eaf74cc958c94ec2a93febe0edf964f8c
mukul <mukulyadav227@gmail.com>
2020-08-13 10:54:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paymentgateway/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.example.paymentgateway;
+
+public class PayPalConfig
+{
+    public static final String PAYPAL_CLIENT_ID = "AchEn8WKZfr83sTvLRxe4zo7-kN9p5dwwuChEW-juKCSbTmfwsqAmtMU6tl2VDJ_dw2evLUJjEheMt7t";
+}
